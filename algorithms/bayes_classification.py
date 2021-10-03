26a6ce7f728c1eb0866629cda1dffdf315300087
KameniAlexNea <Kamenialexnea@gmail.com>
2021-10-03 10:53:09 +0200

    Compute table of likehood

--- algorithms/bayes_classification.py
+++ algorithms/bayes_classification.py
@@ -11,12 +11,13 @@ def count_on_object(x, y, eps=1e-6):
     """
     x = list(x)
     y = list(y)
-    count = collections.defaultdict(collections.defaultdict(int))
+    count = collections.defaultdict(dict)
     for i, j in zip(x, y):
-        count[i][j] += 1
-    df = pd.DataFrame(count) + eps
+        count[i][j] = count[i].get(j, 0) + 1
+    df = pd.DataFrame(count)
     df.fillna(0, inplace=True)
-    col_sum = df.sum(axis=1)
-    df = df/col_sum
+    df = df.T
+    col_sum = df.sum(axis=0)
+    df = df.divide(col_sum) + eps
     df = np.log(df)
     return df
--- tests/test_bayes.py
+++ tests/test_bayes.py
@@ -1,16 +1,17 @@
 
 from algorithms.bayes_classification import count_on_object
-
+import numpy as np
 
 def test_count_on_object():
     x = [1, 1, 1, 2, 3, 3, 4, 4, 4, 4, 5]
     y = [1, 1, 1, 1, 1, 1, 2, 2, 2, 2, 2]
-    data = [
-        [3, 0],
-        [1, 0],
-        [2, 0],
-        [3, 0],
-        [3, 0],
-        [3, 0],
-    ]
+    espected = np.array([
+        [3/6, 0],
+        [1/6, 0],
+        [2/6, 0],
+        [0, 4/5],
+        [0, 1/5],
+    ]) + 1e-6
+    espected = np.log(espected)
     data = count_on_object(x, y, eps=1e-6)
+    assert np.mean(espected == data)==1
