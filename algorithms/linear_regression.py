579ee9e1b052885b7feef676aeda91dc9cad9cdb
KameniAlexNea <Kamenialexnea@gmail.com>
2021-10-06 23:54:48 +0200

    LR : compute loss

--- algorithms/linear_regression.py
+++ algorithms/linear_regression.py
@@ -5,17 +5,29 @@ class LinearRegression:
     def __init__(self, eps=1e-4):
         self.eps = eps
     
+    def compute_loss(self):
+        y_pred = self.predict(self.x)
+        sse = sum((i-j)**2 for i, j in zip(self.y, y_pred))
+        self.sigma = (sse / (len(self.x)-2)) ** (0.5)
+        return self.sigma
+    
     def train(self, train_x:list, train_y:list):
+        self.x = train_x.copy()
+        self.y = train_y.copy()
+
         mean_x = sum(train_x)
         mean_y = sum(train_y)
         mean_xy = sum([i*j for i, j in zip(train_x, train_y)])
         mean_xx = sum([i*i for i in train_x])
         n = len(train_y)
 
-        self.beta1 = (mean_xy - mean_x*mean_y/n) / (mean_xx - mean_x**2/n)
+        self.sxy = mean_xy - mean_x*mean_y/n
+        self.sxx = mean_xx - mean_x**2/n
+
+        self.beta1 = self.sxy / self.sxx
         self.beta0 = (mean_y - mean_x*self.beta1) / n
         return self
 
     def predict(self, test_x):
-        self.preds = [i*self.beta1+self.beta0 for i in test_x]
-        return self.preds.copy()
+        preds = [i*self.beta1+self.beta0 for i in test_x]
+        return preds.copy()
--- examples/linear_regression.py
+++ examples/linear_regression.py
@@ -4,7 +4,7 @@ from algorithms.linear_regression import LinearRegression
 
 def test_linear_regression_algorithm():
     x = np.random.randn(30).tolist()
-    y = [i*2+3+np.random.rand()/100 for i in x]
+    y = [i*2+3+j for i, j in zip(x, (np.random.randn(30)/50).tolist())]
 
     test_x = np.random.randn(10).tolist()
     test_y = [i*2+3 for i in test_x]
@@ -12,4 +12,5 @@ def test_linear_regression_algorithm():
     model = LinearRegression().train(x, y)
     print(test_y)
     print(model.predict(test_x))
-    
+
+    print(model.compute_loss())
