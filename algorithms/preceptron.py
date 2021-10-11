470088e57a57477e7b71e16573609e9b60a41e7c
KameniAlexNea <Kamenialexnea@gmail.com>
2021-10-11 09:04:42 +0200

    test perceptron implementation

--- algorithms/preceptron.py
+++ algorithms/preceptron.py
@@ -7,20 +7,23 @@ class PerceptronClassifier:
 
     def train(self, train_x, train_y):
         encoder = {0: -1, 1: 1, -1: -1}
+        train_y = np.array(train_y).reshape((-1,))
         train_y = np.array([encoder[i] for i in train_y])
-        X = np.c_(range(len(train_y)), train_x)
+        X = np.c_[list(range(len(train_y))), train_x]
         np.random.shuffle(X)
         train_x = X[:, 1:]
-        train_y = train_y[X[:, 0]]
-        W = np.random.randn(train_x.shape[0], 1)
+        train_x = np.c_[np.ones((len(train_y), 1)), train_x]
+        train_y = train_y[[int(i) for i in X[:, 0]]]
+        W = np.random.randn(train_x.shape[1], 1)
         for x, y in zip(train_x, train_y):
-            y_predict = np.sign(np.dot(W, x))
+            y_predict = np.sign(np.dot(x, W))[0]
             if y != y_predict:
-                W = W + y * x.T
+                W = W + y * x.reshape((-1, 1))
         self.W = W
         return self
 
     def predict(self, test_x):
+        test_x = np.c_[np.ones((test_x.shape[0], 1)), test_x]
         preds = np.sign(np.dot(test_x, self.W))
-        preds = np.sign(preds)
+        preds = np.sign(preds).reshape((-1,))
         return preds
--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 from examples.linear_regression import test_linear_regression_algorithm, test_multi_linear_regression_algorithm
 from examples.naive_bayes import test_naive_bayes_algorithm
+from examples.perceptron import test_perceptron_classifier
 
 
 if __name__ == "__main__":
-    test_multi_linear_regression_algorithm()
+    test_perceptron_classifier()
